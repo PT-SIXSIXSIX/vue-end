f1263c63b36ecbc61948ec1f8f24077ca9569ae0
shenaishiren <gjw.jesus@qq.com>
2018-03-15 09:26:59 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
 let globalConfig = {
   cookieExpire: 36000,
   javaUrl: 'http://192.168.2.3:8080/api/v1',
-  nodeJsUrl: 'http://localhost:8081/api/v1',
+  nodeJsUrl: 'http://47.74.176.87:8081/api/v1',
   allowedImageType: ['image/png', 'image/jpeg'],
   bgRouter: [
     '/login', '/register', '/register/two',
